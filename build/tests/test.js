af5b2c9776f9ef4516621cae281937d9b004b4d7
hirwavestine <gilhirwa22@gmail.com>
2019-03-11 13:36:01 +0200

    adding Test sent messages and unread message

--- build/tests/test.js
+++ build/tests/test.js
@@ -12,6 +12,10 @@ var _server = require('../server');
 
 var _server2 = _interopRequireDefault(_server);
 
+var _Message = require('../models/Message');
+
+var _Message2 = _interopRequireDefault(_Message);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _chai2.default.use(_chaiHttp2.default);
@@ -46,6 +50,45 @@ describe('/Post messages', function () {
     _chai2.default.request(_server2.default).post('/api/v1/messages').send(messages).end(function (err, res) {
       res.should.have.status(200);
       res.body.should.be.a('object');
+      done();
+    });
+  });
+});
+/* 
+  *Test the /DELETE/:id route
+  */
+/*describe('/DELETE/: id messages', () =>{
+  it('it should DELETE a message given id', (done) =>{
+    let messages= new Message({id:1234, subject:"Welcome to EPIC-Mail",message:"Andela Challenge",
+    parentMessageId:5678, status: "sent",createdOn: 10032019
+   })
+   messages.save((err, messages)=>{
+     chai.request(server)
+     .delete('/api/v1/messages' + messages.id)
+     .end((err, res) => {
+       res.should.have.status(200);
+       res.body.should.be.a('object');
+       (done);
+     });
+    });
+  });
+});
+  */
+describe('/Get Unread messages', function () {
+  it('it should GET all Unread messages', function (done) {
+    _chai2.default.request(_server2.default).get('/api/v1/messages/unread').end(function (err, res) {
+      res.body.should.be.a('array');
+      done();
+    });
+  });
+});
+/*
+GET SENT MESSAGES
+*/
+describe('/Get Sent messages', function () {
+  it('it should GET all Sent messages', function (done) {
+    _chai2.default.request(_server2.default).get('/api/v1/messages/sent').end(function (err, res) {
+      res.body.should.be.a('array');
       done();
     });
   });
--- server/tests/test.js
+++ server/tests/test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server';
+import Message from '../models/Message';
 chai.use(chaiHttp);
 chai.should();
 
@@ -42,6 +43,50 @@ chai.should();
       });
     });
   });
-   
+/* 
+  *Test the /DELETE/:id route
+  */
+ /*describe('/DELETE/: id messages', () =>{
+   it('it should DELETE a message given id', (done) =>{
+     let messages= new Message({id:1234, subject:"Welcome to EPIC-Mail",message:"Andela Challenge",
+     parentMessageId:5678, status: "sent",createdOn: 10032019
+    })
+    messages.save((err, messages)=>{
+      chai.request(server)
+      .delete('/api/v1/messages' + messages.id)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        (done);
+      });
+
+    });
+   });
+ });
+   */
+ describe('/Get Unread messages', () => {
+  it('it should GET all Unread messages', (done) => {
+    chai.request(server)
+        .get('/api/v1/messages/unread')
+        .end((err, res) => {
+              res.body.should.be.a('array');
+          done();
+        });
+  });
+});
+/*
+GET SENT MESSAGES
+*/
+describe('/Get Sent messages', () => {
+  it('it should GET all Sent messages', (done) => {
+    chai.request(server)
+        .get('/api/v1/messages/sent')
+        .end((err, res) => {
+              res.body.should.be.a('array');
+          done();
+        });
+  });
+});
+
 
 
